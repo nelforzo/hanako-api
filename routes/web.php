83494c4a730407be86faec863c14f9652e99a10c
Frank Lopez <spiccanelforzo@gmail.com>
2021-05-13 22:09:12 +0900

    updated web root route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'this is the web root for hanako-api. the stuff you\'re looking for is under /api. go have fun.';
 });
