5274d8ad5f8dec610093fa9b1902a5663e3e6372
Frank Lopez <spiccanelforzo@gmail.com>
2021-11-13 18:58:52 +0900

    added request validator

--- app/Services/RatingService.php
+++ app/Services/RatingService.php
@@ -2,22 +2,73 @@
 
 namespace App\Services;
 
-use App\Models\StuffRatings;
+use App\Models\Packages;
+use App\Models\Ratings;
+use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class RatingService {
   //create or update rating
-  public function rateStuff($stuffId, $rating, $comment) {
-    $rating = StuffRatings::updateOrCreate(
-      ['stuff_id' => $stuffId],
-      ['rating' => $rating, 'comment' => $comment]
+  public function createRating(Request $request) {
+
+    //validation
+    $validator = Validator::make($request->all(), [
+      'package_id' => 'required',
+      'rating' => 'required|between:0,10',
+    ]);
+
+    if ($validator->fails()) {
+      return $validator->errors();
+    }
+
+    $rating = Ratings::updateOrCreate(
+      ['package_id' => $request->input('package_id')],
+      ['rating' => $request->input('rating'), 'comment' => $request->input('comment')]
     );
     return $rating;
   }
 
   //delete stuff rating
-  public function deleteRating($stuffId) {
-    return StuffRatings::where('stuff_id', $stuffId)->delete();
+  public function deleteRating(Request $request) {
+
+    //validation
+    $validator = Validator::make($request->all(), [
+      'package_id' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return $validator->errors();
+    }
+
+    return Ratings::where('package_id', $request->input('package_id'))->delete();
+  }
+
+  // public function getCategoryRating(Request $request) {
+  //   //validation
+  //   $validator = Validator::make($request->all(), [
+  //     'category_id' => 'required',
+  //   ]);
+  //   if ($validator->fails()) {
+  //     return $validator->errors();
+  //   }
+  //   //get packages in category
+  //   $packages = Packages::where('category_id', $request->input('category_id'))->get('id');
+  //   return Ratings::whereIn('package_id', $packages)->avg('rating');
+  // }
+
+  public function getRatingByPackageId(Request $request) {
+    
+    //validation
+    $validator = Validator::make($request->all(), [
+      'package_id' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return $validator->errors();
+    }
+
+    Return Ratings::where('package_id', $request->input('package_id'))->get('rating');
   }
 }
